bb00e8577ac809b8d3a25c89239ad9a8d0b28383
feras10alali <163681534+feras10alali@users.noreply.github.com>
2025-05-06 22:45:24 +0300

    speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { Header, Footer } from '$lib/components'
 	import '../css/global.css'
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 </script>
 
 <div class='app'>
